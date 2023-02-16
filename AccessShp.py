ea1c30c7b58c597af2dc95e55e89b0eebb9e0406
raunak220191 <90885744+raunak220191@users.noreply.github.com>
2023-02-16 11:31:33 +0530

    Update AccessShp.py

--- AccessShp.py
+++ AccessShp.py
@@ -1,32 +1,27 @@
 import requests
+import adal
+import json
 
 # Replace the placeholders with your own values
 site_url = 'https://your-sharepoint-site.sharepoint.com'
-file_url = 'https://your-sharepoint-site.sharepoint.com/sites/your-site/your-library/your-file.docx'
-username = 'your-username'
-password = 'your-password'
+library_path = '/sites/your-site/your-library/'
+file_name = 'your-file.docx'
+tenant_id = 'your-tenant-id'
+client_id = 'your-client-id'
+client_secret = 'your-client-secret'
 
-# Get the authentication token
-auth_url = f'{site_url}/_api/contextinfo'
-headers = {'accept': 'application/json;odata=verbose'}
-response = requests.post(auth_url, headers=headers, auth=(username, password))
-response_data = response.json()
-form_digest_value = response_data['d']['GetContextWebInformation']['FormDigestValue']
+# Get an access token
+authority_url = 'https://login.microsoftonline.com/' + tenant_id
+resource_url = site_url
+context = adal.AuthenticationContext(authority_url)
+token = context.acquire_token_with_client_credentials(resource_url, client_id, client_secret)
 
 # Download the file
-file_data = requests.get(file_url, headers={
-    'accept': 'application/json;odata=verbose',
-    'Authorization': f'Bearer {form_digest_value}'
-}, auth=(username, password))
+file_url = site_url + library_path + file_name
+response = requests.get(file_url, headers={'Authorization': 'Bearer ' + token['accessToken']}, stream=True)
 
 # Save the file to disk
-with open('your-file.docx', 'wb') as f:
-    f.write(file_data.content)
-
-###In this example, the code first gets an authentication token from SharePoint using the _api/contextinfo endpoint. It then uses this token to download the file from the given file URL. Finally, the downloaded file is saved to the local disk.
-
-###Make sure to replace the placeholders with your own values before running the code. Also note that you will need to have the requests library installed in your Python environment.
-
-
-
-
+with open(file_name, 'wb') as f:
+    for chunk in response.iter_content(chunk_size=1024):
+        if chunk:
+            f.write(chunk)
