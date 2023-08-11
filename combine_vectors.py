4453eb5e90e941b38745faac9becf6933d09b16f
raunak220191 <90885744+raunak220191@users.noreply.github.com>
2023-08-11 11:42:12 +0530

    Update combine_vectors.py

--- combine_vectors.py
+++ combine_vectors.py
@@ -1,37 +1,23 @@
 import os
 import faiss
-import pickle
 
 # List of folders containing FAISS index files
 index_folders = ['folder1', 'folder2', 'folder3']  # Update with your folder names
 
 # Initialize an empty combined index
-combined_index = None
+d = 256  # Dimension of your vectors
+index = faiss.IndexFlatL2(d)  # Use IndexFlatL2 for example
 
 # Iterate through each folder
 for folder in index_folders:
-    # Load index from .pkl and .faiss files
-    index_path = os.path.join(folder, 'index.pkl')
+    # Load the vectors from .faiss file
     vectors_path = os.path.join(folder, 'vectors.faiss')
+    loaded_index = faiss.read_index(vectors_path)
     
-    with open(index_path, 'rb') as index_file:
-        index = pickle.load(index_file)
-    
-    vectors = faiss.read_index(vectors_path)
-    
-    # Append or merge the vectors and metadata
-    if combined_index is None:
-        combined_index = index
-    else:
-        combined_index.merge_from(index)
-    
-    combined_index.add(vectors)
+    # Add vectors from the loaded index to the combined index
+    vectors = loaded_index.reconstruct_n(loaded_index.ntotal)
+    index.add(vectors)
 
-# Save the combined index to new .pkl and .faiss files
-combined_index_path = 'combined_index.pkl'
-combined_vectors_path = 'combined_vectors.faiss'
-
-with open(combined_index_path, 'wb') as combined_index_file:
-    pickle.dump(combined_index, combined_index_file)
-
-faiss.write_index(combined_index, combined_vectors_path)
+# Save the combined index to a new .faiss file
+combined_index_path = 'combined_index.faiss'
+faiss.write_index(index, combined_index_path)
